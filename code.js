14d5fd712d3269bd6e09f8e277dce40089939b43
Casey Rodriguez <itscaseyr@gmail.com>
2021-05-09 20:01:46 -0400

    Added prototype changes

--- code.js
+++ code.js
@@ -1,6 +1,9 @@
+dpi_x = prompt("Please enter your device DPI (this is 96 on most desktops, but varies greatly on mobile devices): ")
+document.getElementById('testdiv').innerHTML = "DPI: " + dpi_x.toString()
+screenWidth = document.getElementById('sizer').offsetWidth;
+screenHeight = document.getElementById('sizer').offsetHeight;
 
-
-DPIofYourDeviceScreen = 323; //you will need to measure or look up the DPI or PPI of your device/browser to make sure you get the right scale!!
+DPIofYourDeviceScreen = dpi_x; //you will need to measure or look up the DPI or PPI of your device/browser to make sure you get the right scale!!
 createCanvasOfInputArea = DPIofYourDeviceScreen*1; //aka, 1.0 inches square!
 
 let totalTrialNum = 2; //the total number of phrases to be tested - set this low for testing. Might be ~10 for the real bakeoff!
@@ -30,7 +33,7 @@ const topHundo = ["the","of","to","and","a","in","is","it","you","that","he","wa
 function setup()
 {
   textSize(12*scaleFactor);
-  createCanvas(828,1792); //Sets the createCanvas of the app. You should modify this to your device's native createCanvas. Many phones today are 1080 wide by 1920 tall.
+  createCanvas(0.9*screenWidth, 0.9*screenHeight); //Sets the createCanvas of the app. You should modify this to your device's native createCanvas. Many phones today are 1080 wide by 1920 tall.
   noStroke(); //my code doesn't use any strokes.
   predictionary.addWords(topHundo);
   phrases=shuffle(phrases)
@@ -46,18 +49,18 @@ function draw()
   {
     fill(0);
     textAlign(CENTER);
-    text("Trials complete!",400,200); //output
-    text("Total time taken: " + (finishTime - startTime),400,200+20*scaleFactor); //output
-    text("Total letters entered: " + lettersEnteredTotal,400,200+40*scaleFactor); //output
-    text("Total letters expected: " + lettersExpectedTotal,400,200+60*scaleFactor); //output
-    text("Total errors entered: " + errorsTotal,400,200+80*scaleFactor); //output
+    text("Trials complete!",width/2,200); //output
+    text("Total time taken: " + (finishTime - startTime),width/2,200+20*scaleFactor); //output
+    text("Total letters entered: " + lettersEnteredTotal,width/2,200+40*scaleFactor); //output
+    text("Total letters expected: " + lettersExpectedTotal,width/2,200+60*scaleFactor); //output
+    text("Total errors entered: " + errorsTotal,width/2,200+80*scaleFactor); //output
     let wpm = (lettersEnteredTotal/5.0)/((finishTime - startTime)/60000); //FYI - 60K is number of milliseconds in minute
-    text("Raw WPM: " + wpm,400,200+100*scaleFactor); //output
+    text("Raw WPM: " + wpm,width/2,200+100*scaleFactor); //output
     let freebieErrors = lettersExpectedTotal*.05; //no penalty if errors are under 5% of chars
-    text("Freebie errors: " + nf(freebieErrors,1,3),400,200+120*scaleFactor); //output
+    text("Freebie errors: " + nf(freebieErrors,1,3),width/2,200+120*scaleFactor); //output
     let penalty = max(errorsTotal-freebieErrors, 0) * .5;
-    text("Penalty: " + penalty,400,200+140*scaleFactor);
-    text("WPM w/ penalty: " + (wpm-penalty),400,200+160*scaleFactor); //yes, minus, because higher WPM is better
+    text("Penalty: " + penalty,width/2,200+140*scaleFactor);
+    text("WPM w/ penalty: " + (wpm-penalty),width/2,200+160*scaleFactor); //yes, minus, because higher WPM is better
 
     return;
   }
@@ -86,24 +89,66 @@ function draw()
     //you can very slightly adjust the position of the target/entered phrases and next button
     textAlign(LEFT); //align the text left
     fill(128);
-    text("Phrase " + (currTrialNum+1) + " of " + totalTrialNum, 70, 50); //draw the trial count
+    text("Phrase " + (currTrialNum+1) + " of " + totalTrialNum, width/2-100, height/2-3*createCanvasOfInputArea); //draw the trial count
     fill(128);
-    text("Target:   " + currentPhrase, 70, 100); //draw the target string
-    text("Entered:  " + currentTyped + "|", 70, 140); //draw what the user has entered thus far 
+    text("Target:   " + currentPhrase, width/2-100, height/2-3*createCanvasOfInputArea+20); //draw the target string
+    text("Entered:  " + currentTyped + "|", width/2-100, height/2-3*createCanvasOfInputArea+40); //draw what the user has entered thus far 
     //draw very basic next button
-    fill(255, 0, 0);
-    rect(600, 600, 200, 200); //draw next button
+    if (currentPhrase == currentTyped) {
+      fill(0, 255, 0);
+    }
+    else {
+      fill(255, 0, 0);
+    }
+    rect(width/2+createCanvasOfInputArea/2, height/2-createCanvasOfInputArea/2, createCanvasOfInputArea, createCanvasOfInputArea); //draw next button
     fill(255);
-    text("NEXT > ", 650, 650); //draw next label
+    text("NEXT > ", width/2+3*createCanvasOfInputArea/4, height/2); //draw next label
 
-        fill(200);
+    fill(200);
     for(row = 0; row < 5; row++) {
         for(col = 0; col < 6; col++) {
-            if(row == 4 && col > 3) continue;
-            rect(width/2-createCanvasOfInputArea/2+col*createCanvasOfInputArea/6, height/2-createCanvasOfInputArea/2+row*createCanvasOfInputArea/6, createCanvasOfInputArea/6, createCanvasOfInputArea/6)
-            fill(0)
-            text(String.fromCharCode(row*6+col+95), width/2-createCanvasOfInputArea/2+col*createCanvasOfInputArea/6+createCanvasOfInputArea/20, height/2-createCanvasOfInputArea/2+row*createCanvasOfInputArea/6+createCanvasOfInputArea/20, createCanvasOfInputArea/6, createCanvasOfInputArea/6)
-            fill(200)
+            // if mouse pressed within button, change fill
+            if(mouseIsPressed && didMouseClick(width/2-createCanvasOfInputArea/2+col*createCanvasOfInputArea/6, height/2-createCanvasOfInputArea/2+row*createCanvasOfInputArea/6, createCanvasOfInputArea/6, createCanvasOfInputArea/6)) {
+              fill(0, 127, 0);
+            }
+            else {
+              fill(200);
+            }
+            if(row == 4 && col == 0) continue;
+            else if(row == 4 && col == 1) { 
+              if(mouseIsPressed && didMouseClick(width/2-createCanvasOfInputArea/2+col*createCanvasOfInputArea/6-createCanvasOfInputArea/6, height/2-createCanvasOfInputArea/2+row*createCanvasOfInputArea/6, createCanvasOfInputArea/3, createCanvasOfInputArea/6)) {
+                fill(0, 127, 0);
+              }
+              else {
+                fill(150);
+              }
+              rect(width/2-createCanvasOfInputArea/2+col*createCanvasOfInputArea/6-createCanvasOfInputArea/6, height/2-createCanvasOfInputArea/2+row*createCanvasOfInputArea/6, createCanvasOfInputArea/3, createCanvasOfInputArea/6);
+              fill(0);
+              text('SPC', width/2-createCanvasOfInputArea/2+col*createCanvasOfInputArea/6+createCanvasOfInputArea/20-createCanvasOfInputArea/6, height/2-createCanvasOfInputArea/2+row*createCanvasOfInputArea/6+createCanvasOfInputArea/20, createCanvasOfInputArea/6, createCanvasOfInputArea/6);
+              continue;
+            }
+            else if(row ==4 && col == 4) continue;
+            else if(row == 4 && col == 5) { 
+              if(mouseIsPressed && didMouseClick(width/2-createCanvasOfInputArea/2+col*createCanvasOfInputArea/6-createCanvasOfInputArea/6, height/2-createCanvasOfInputArea/2+row*createCanvasOfInputArea/6, createCanvasOfInputArea/3, createCanvasOfInputArea/6)) {
+                fill(0, 127, 0);
+              }
+              else {
+                fill(150);
+              }
+              rect(width/2-createCanvasOfInputArea/2+col*createCanvasOfInputArea/6-createCanvasOfInputArea/6, height/2-createCanvasOfInputArea/2+row*createCanvasOfInputArea/6, createCanvasOfInputArea/3, createCanvasOfInputArea/6);
+              fill(0);
+              text('BCK', width/2-createCanvasOfInputArea/2+col*createCanvasOfInputArea/6+createCanvasOfInputArea/20-createCanvasOfInputArea/6, height/2-createCanvasOfInputArea/2+row*createCanvasOfInputArea/6+createCanvasOfInputArea/20, createCanvasOfInputArea/6, createCanvasOfInputArea/6);
+              continue;
+            }
+            else if(row == 4) {
+              rect(width/2-createCanvasOfInputArea/2+col*createCanvasOfInputArea/6, height/2-createCanvasOfInputArea/2+row*createCanvasOfInputArea/6, createCanvasOfInputArea/6, createCanvasOfInputArea/6);  
+              fill(0);
+              text(String.fromCharCode(row*6+col+95), width/2-createCanvasOfInputArea/2+col*createCanvasOfInputArea/6+createCanvasOfInputArea/20, height/2-createCanvasOfInputArea/2+row*createCanvasOfInputArea/6+createCanvasOfInputArea/20, createCanvasOfInputArea/6, createCanvasOfInputArea/6);
+              continue;
+            }
+            rect(width/2-createCanvasOfInputArea/2+col*createCanvasOfInputArea/6, height/2-createCanvasOfInputArea/2+row*createCanvasOfInputArea/6, createCanvasOfInputArea/6, createCanvasOfInputArea/6);
+            fill(0);
+            text(String.fromCharCode(row*6+col+97), width/2-createCanvasOfInputArea/2+col*createCanvasOfInputArea/6+createCanvasOfInputArea/20, height/2-createCanvasOfInputArea/2+row*createCanvasOfInputArea/6+createCanvasOfInputArea/20, createCanvasOfInputArea/6, createCanvasOfInputArea/6);
         }
     }
     textAlign(CENTER);
@@ -114,7 +159,7 @@ function draw()
     rect(width/2-createCanvasOfInputArea/2, height/2-createCanvasOfInputArea/2+5*createCanvasOfInputArea/6, createCanvasOfInputArea, createCanvasOfInputArea/6)
     stroke(0)
 
-    fill(0)
+    fill(255)
     text(predicts[0], width/2, height/2+createCanvasOfInputArea/2.2);
     
     console.log
@@ -140,23 +185,34 @@ function singleTap()
   }
   for(row = 0; row < 5; row++) {
     for(col = 0; col < 6; col++) {
-        if(didMouseClick(width/2-createCanvasOfInputArea/2+col*createCanvasOfInputArea/6, height/2-createCanvasOfInputArea/2+row*createCanvasOfInputArea/6, createCanvasOfInputArea/6, createCanvasOfInputArea/6)) {
-            currentLetter = String.fromCharCode(row*6+col+95);
-            if (currentLetter=='_') //if underscore, consider that a space bar
-            currentTyped = currentTyped + " ";
-            else if (currentLetter=='`' & currentTyped.length>0) //if `, treat that as a delete command
-            currentTyped = currentTyped.substring(0, currentTyped.length-1);
-            else if (currentLetter!='`') //if not any of the above cases, add the current letter to the typed string
-            currentTyped = currentTyped + currentLetter;
-            }
+      if(didMouseClick(width/2-createCanvasOfInputArea/2+col*createCanvasOfInputArea/6, height/2-createCanvasOfInputArea/2+row*createCanvasOfInputArea/6, createCanvasOfInputArea/6, createCanvasOfInputArea/6)) {
+        if(row == 4 && col < 2)
+          currentLetter = "_";
+        else if(row == 4 && col == 2)
+          currentLetter = 'y';
+        else if(row == 4 && col == 3)
+          currentLetter = 'z';
+        else if(row == 4 && col > 3)
+          currentLetter = '`';
+        else
+          currentLetter = String.fromCharCode(row*6+col+97);
+        if (currentLetter=='_') //if underscore, consider that a space bar
+          currentTyped = currentTyped + " ";
+        else if (currentLetter=='`' & currentTyped.length>0) //if `, treat that as a delete command
+          currentTyped = currentTyped.substring(0, currentTyped.length-1);
+        else if (currentLetter!='`') //if not any of the above cases, add the current letter to the typed string
+          currentTyped = currentTyped + currentLetter;
+      }
     }
-}
+  }
 
+  //clicked in word prediction box
   if(didMouseClick(width/2-createCanvasOfInputArea/2, height/2-createCanvasOfInputArea/2+5*createCanvasOfInputArea/6, createCanvasOfInputArea, createCanvasOfInputArea/6)) {
     currentTyped = shorten(currentTyped.split(" ")).join(" ") + " " + predicts[0] + String(" ");
   } 
+
   //You are allowed to have a next button outside the 1" area
-  if (didMouseClick(600, 600, 200, 200)) //check if click is in next button
+  if (didMouseClick(width/2+createCanvasOfInputArea/2, height/2-createCanvasOfInputArea/2, createCanvasOfInputArea, createCanvasOfInputArea)) //check if click is in next button
   {
     nextTrial(); //if so, advance to next trial
   }
